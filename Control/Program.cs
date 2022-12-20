e2efbd56b8bdab7113dd6b40e3ae60bd37062eb0
Niko2405 <nilskornmann@gmail.com>
2022-12-20 18:41:45 +0100

    Add Configs

--- Control/Program.cs
+++ Control/Program.cs
@@ -2,8 +2,16 @@
 {
 	internal class Program
 	{
+		private static void Start()
+		{
+
+		}
 		static void Main(string[] args)
 		{
+			for (int i = 0; i < args.Length; i++)
+			{
+				Console.WriteLine(args[i]);
+			}
 			// Init
 			// Auf Start Argumente warten
 			// Programm dazu öffnen (Serial Test, Socket Test...)
---
+++ Control/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+	internal class Server
+	{
+		public const string DIR_MAIN = "data/";
+		public const string DIR_LOGS = DIR_MAIN + "logs/";
+		public const string DIR_USER = DIR_MAIN + "usr/";
+		public const string DIR_CACHE = DIR_MAIN + "cache/";
+		public const string DIR_CONFIGS = DIR_MAIN + "configs/";
+		public const string DIR_VOICE = DIR_MAIN + "voices/";
+		public const string DIR_X = null;
+
+		public static bool enabled = false;
+	}
+}
